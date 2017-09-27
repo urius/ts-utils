d7cf6b14d4a844a3360b0e903de733a7a77c7f0e
is-up <isup89@gmail.com>
2017-09-28 01:54:01 +0300

    location style + check origin object

--- Utils.ts
+++ Utils.ts
@@ -17,21 +17,21 @@ export module Utils {
         if(typeof(target) == "object") {
             if(Array.isArray(target) == Array.isArray(origin)) {
                 if(Array.isArray(target) && Array.isArray(origin)) {
-                    if(target.length > 0 && origin) {
+                    if(target.length > 0 && origin.length > 0) {
                         let subResult:ICompareJSONResult = compareJSONSchemas(target[0], origin[0]);
                         if(subResult.success == false) {
                             result.success == false;
-                            result.location += "[" + subResult.location + "]";
+                            result.location += "[]" + (subResult.location.length > 0 ? ("." + subResult.location) : "");
                             return result;
                         }
                         return result;
                     }
                 } 
-                for (let prop in target) {
+                for (let prop in origin) {
                     let subResult:ICompareJSONResult = compareJSONSchemas(target[prop], origin[prop]);
                     if(subResult.success == false) {
                         result.success = false;
-                        result.location += "{" + prop + ":" + subResult.location + "}";
+                        result.location += prop + (subResult.location.length > 0 ? ("." + subResult.location) : "");
                         return result;
                     }
                 }
